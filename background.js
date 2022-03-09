9f8c38b6b7a41814b0b147de3b2d1b750b579e47
ctheara <59755034+ctheara@users.noreply.github.com>
2022-03-09 01:54:55 -0500

    update js file
    use jQuery to swap all image on page

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
 // To inject jQuery onto a page 
+
+
+//chrome.tabs.onUpdated.addListener(function(tab) {
+    chrome.browserAction.onClicked.addListener(function(tab) {
+
+        // inject jQuery, then execute hide.js on callback
+        chrome.tabs.executeScript({
+           file: 'jquery-3.6.0.min.js'
+               }, function(x) {                   // callback function (parameter is required even if unused) 
+                       chrome.tabs.executeScript({
+                           file: "swapImage.js" 
+                       });
+        });
+   
+       
+   });
+   
--- swapImage.js
+++ swapImage.js
@@ -1,1 +1,7 @@
 //swap all image
+var imgURL = chrome.extension.getURL("images/kfp2.jpg");
+
+
+$('img').each(function() {
+    $('img').attr('src',imgURL);
+});
